5b7c1bfbd94c9623273680295a322863e2acca7e
hamdivazim <coding.wizard4@gmail.com>
2023-04-10 21:53:18 +0100

    Create initial program

---
+++ django_made_easy.py
@@ -0,0 +1,37 @@
+# Django Made Easy
+# https://github.com/hamdivazim/Django-Made-Easy
+#
+#
+# Copyright Hamd Waseem 2023
+# This project is licensed under the Apache License 2.0
+#
+
+import sys, os
+
+def pip_cmd(cmd):
+    os.system(Rf"virt\Scripts\pip.exe {cmd}")
+
+def get_args():
+    return sys.argv[1:]
+
+def set_py_name():
+    global PYTHON_NAME
+    if os.system("python --version") != "0":
+        PYTHON_NAME = "py"
+
+PYTHON_NAME = "python"
+
+def main():
+    set_py_name()
+
+    print("Creating virtual environment...")
+    os.system(f"{PYTHON_NAME} -m venv virt")
+
+    print("Installing django..")
+    pip_cmd("install django")
+
+    print(f"Creating project named {get_args()[0]} ...")
+    os.system(Rf"virt\Scripts\django-admin.exe startproject {get_args()[0]}")
+
+if __name__ == "__main__":
+    main()
